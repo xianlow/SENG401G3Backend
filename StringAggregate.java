fb72f0c81f81f62395412a29ba7fc88ede3e9eb7
JackWMclean <102626729+JackWMclean@users.noreply.github.com>
2023-02-27 20:13:47 -0700

    Handles commands and generates events

---
+++ StringAggregate.java
@@ -0,0 +1,35 @@
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+public class StringAggregate {
+    private final String id;
+    private String value;
+
+    public StringAggregate(String id) {
+        this.id = id;
+        this.value = "";
+    }
+
+    public void apply(StringChangedEvent event) {
+        if (!event.getId().equals(id)) {
+            throw new IllegalArgumentException("Event with incorrect ID passed to StringAggregate");
+        }
+        this.value = event.getNewValue();
+    }
+
+    public List<StringChangedEvent> changeString(String newValue, LocalDateTime timestamp) {
+        StringChangedEvent event = new StringChangedEvent(id, this.value, newValue, timestamp);
+        apply(event);
+        return Collections.singletonList(event);
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public List<StringChangedEvent> handle(ChangeStringCommand command, LocalDateTime timestamp) {
+        List<StringChangedEvent> events = changeString(command.getNewValue(), timestamp);
+        return events;
+    }
+}
