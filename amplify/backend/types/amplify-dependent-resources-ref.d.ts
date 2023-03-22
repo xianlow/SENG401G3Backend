677fcc2c9de8685ff06c89bc84726f1afb4a6266
Xian Wei Low <xwei.low@gmail.com>
2023-03-21 20:29:47 -0600

    SN-18 <Added simple files for amplify backend>

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "rhapsodyRest": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "rhapsodyLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
