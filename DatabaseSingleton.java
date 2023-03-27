d03968ea477e4b5b458ac56283c53552abd840e4
JackWMclean <102626729+JackWMclean@users.noreply.github.com>
2023-03-26 22:13:35 -0600

    Create DatabaseSingleton.java

---
+++ DatabaseSingleton.java
@@ -0,0 +1,48 @@
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class DatabaseSingleton {
+    private static DatabaseSingleton onlyInstance;
+    private AccessDatabase access;
+    private DatabaseSingleton(){
+        access = new AccessDatabase("jdbc:mysql://localhost:3306/db", "test", "password");
+    }
+
+    public static DatabaseSingleton getOnlyInstance(){
+        if(onlyInstance == null){
+            onlyInstance = new DatabaseSingleton();
+        }
+        return onlyInstance;
+    }
+
+    public static void setOnlyInstance(DatabaseSingleton onlyInstace){
+        DatabaseSingleton.onlyInstance = onlyInstace;
+    }
+
+    public ArrayList<String> getPosts(String id){
+        ArrayList<String> tmp = new ArrayList<>();
+        access.initializeConnection();
+        tmp = access.getPosts(id);
+        access.dbConnectClose();
+        return tmp;
+    }
+    public void updatePost(String val){
+        access.initializeConnection();
+        access.updatePost(val);
+        access.dbConnectClose();
+        
+    }
+    public Void removePost(String val){
+        access.initializeConnection();
+        access.removePost(val);
+        access.dbConnectClose();
+        
+    }
+    public void addPost(String id, String val, LocalDate timestamp){
+        access.initializeConnection();
+        access.addPost(id, val, timestamp);
+        access.dbConnectClose();
+    }
+}
